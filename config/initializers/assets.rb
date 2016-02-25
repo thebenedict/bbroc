abbdb196cefbf7c46a96e726d7b5556695173c0f
thebenedict <thebenedict@gmail.com>
2016-02-25 13:13:28 +0300

    add favicons, meta tags, other header info

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,6 +5,9 @@ Rails.application.config.assets.version = (ENV["ASSETS_VERSION"] || "1.0")
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
+Dir.glob("#{Rails.root}/app/assets/images/**/").each do |path|
+  Rails.application.config.assets.paths << path
+end
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
