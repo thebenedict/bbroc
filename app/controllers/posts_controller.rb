8cc9a1e1aa2d25a183ce3baed60be7ffbf806365
thebenedict <thebenedict@gmail.com>
2016-02-29 22:10:13 +0300

    unstyled post form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,24 @@ class PostsController < ApplicationController
   def index
     # preserve form state details if user sign up fails
     @resource = User.new(session[:incomplete_resource])
-    @posts = Post.all
+    @new_post = Post.new
+    @posts = Post.order(created_at: :desc)
+  end
+
+  def create 
+    post = current_user.posts.build(post_params)
+    if post.save
+      flash.notice = "Success, thanks for posting!"
+    else
+      flash.alert = "Something went wrong, please try again."
+    end
+    redirect_to root_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:item_id, :vendor_id)
   end
 end
 
--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,2 +1,3 @@
 class Vendor < ActiveRecord::Base
+  validates :name, uniqueness: true
 end
