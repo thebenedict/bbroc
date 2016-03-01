134629bb934e6b8afcaadfeb3d6b9eeb854c7935
Half Man Half Amazing <ericmpadron@gmail.com>
2016-03-01 15:52:02 +0300

    add styles to sign in/up forms

--- app/assets/javascripts/site-nav.js
+++ app/assets/javascripts/site-nav.js
@@ -1,11 +1,11 @@
 // navigation
 $('.nav a').on('click', function() {
-  var className = $(this).attr('class'); 
-  var panelId = '#' + className; 
+  var className = $(this).attr('class');
+  var panelId = '#' + className;
   $(this).add(panelId).siblings().removeClass('active');
   $(this).add(panelId).addClass('active');
 });
- 
+
 $('body').on('swiperight', function() {
   // check to see if on middle or left panel
   if (($('.middle').hasClass('active')) || ($('.left').hasClass('active')) ) {
@@ -15,10 +15,10 @@ $('body').on('swiperight', function() {
       // must be on right panel, slide to middle
       $('#left, .left, #right, .right, .menu-indicator').removeClass('active');
       $('.middle').addClass('active');
-    }  
+    }
 });
 
-$('body').on('swipeleft', function() { 
+$('body').on('swipeleft', function() {
   // check to see if on middle or right panel
   if (($('.middle').hasClass('active')) || ($('.right').hasClass('active')) ) {
   $('#left, .left, .middle').removeClass('active');
@@ -27,10 +27,9 @@ $('body').on('swipeleft', function() {
       // must be on left panel, slide to middle
       $('#left, .left, #right, .right').removeClass('active');
       $('.middle').addClass('active');
-    }   
+    }
 });
 
-// toggles
-$('#email-signup').on('click', function() {
-  $('#new_user').toggleClass('active');
+$('.toggle-container a').on('click', function() {
+  $('#top-container .post').toggleClass('hidden');
 });
