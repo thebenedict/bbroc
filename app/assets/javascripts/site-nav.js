31a7db5453e300770e2717bf1455ae44c0a31fee
Half Man Half Amazing <ericmpadron@gmail.com>
2016-03-04 14:34:55 +0300

    new post button and sign up/in layout

--- app/assets/javascripts/site-nav.js
+++ app/assets/javascripts/site-nav.js
@@ -6,31 +6,7 @@ $('.nav a').on('click', function() {
   $(this).add(panelId).addClass('active');
 });
 
-$('body').on('swiperight', function() {
-  // check to see if on middle or left panel
-  if (($('.middle').hasClass('active')) || ($('.left').hasClass('active')) ) {
-    $('#right, .right, #bottom, .middle').removeClass('active');
-    $('#left, .left').addClass('active');
-  } else {
-      // must be on right panel, slide to middle
-      $('#left, .left, #right, .right, .menu-indicator').removeClass('active');
-      $('.middle').addClass('active');
-    }
-});
-
-$('body').on('swipeleft', function() {
-  // check to see if on middle or right panel
-  if (($('.middle').hasClass('active')) || ($('.right').hasClass('active')) ) {
-  $('#left, .left, .middle').removeClass('active');
-  $('#right, .right').addClass('active');
-  } else {
-      // must be on left panel, slide to middle
-      $('#left, .left, #right, .right').removeClass('active');
-      $('.middle').addClass('active');
-    }
-});
-
 // sign in and sign up toggle
-$('.toggle-container a').on('click', function() {
-  $('#top-container .post').toggleClass('hidden');
+$('.toggle-container span a').on('click', function() {
+  $('.sign-up, .sign-in').toggleClass('hidden');
 });
