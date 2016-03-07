ebac239913871d20130d3cfc97b634658eb40903
Half Man Half Amazing <ericmpadron@gmail.com>
2016-03-07 18:56:57 +0300

    new front page

--- app/assets/javascripts/site-nav.js
+++ app/assets/javascripts/site-nav.js
@@ -1,11 +1,3 @@
-// navigation
-// $('.nav a').on('click', function() {
-//   var className = $(this).attr('class');
-//   var panelId = '#' + className;
-//   $(this).add(panelId).siblings().removeClass('active');
-//   $(this).add(panelId).addClass('active');
-// });
-
 // sign in and sign up toggle
 $('.toggle-container span a').on('click', function() {
   $('.sign-up, .sign-in').toggleClass('hidden');
--- app/controllers/extra_pages_controller.rb
+++ app/controllers/extra_pages_controller.rb
@@ -1,6 +1,5 @@
 class ExtraPagesController < ApplicationController
   def home
     @posts = Post.limit(10)
-    render layout: 'bare', template: 'extra_pages/home'
   end
 end
