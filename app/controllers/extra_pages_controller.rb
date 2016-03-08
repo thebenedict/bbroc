7ce73f5c797b4ba9d4be6c8cfa0b3f46f3ce05c4
thebenedict <thebenedict@gmail.com>
2016-03-08 13:07:39 +0300

    prevent logged-in users from seeing the home page

--- app/controllers/extra_pages_controller.rb
+++ app/controllers/extra_pages_controller.rb
@@ -1,5 +1,13 @@
 class ExtraPagesController < ApplicationController
+  before_action :redirect_if_logged_in
+
   def home
     @posts = Post.limit(10)
   end
+
+  private
+
+    def redirect_if_logged_in
+      redirect_to posts_path if current_user
+    end
 end
