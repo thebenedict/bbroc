4f4838e65b4d2f2f660f21dc4c95b6042a1681cc
Half Man Half Amazing <ericmpadron@gmail.com>
2016-03-08 13:12:25 +0300

    canvas dim change

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,4 +7,9 @@ class Post < ActiveRecord::Base
 
   has_attached_file :image, styles: { default: "305x229#"}, default_url: "apples-placeholder"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
+  def safe_post_item
+    self.item.name.split(" ")[0]
+  end
+
 end
