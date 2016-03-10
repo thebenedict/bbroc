4ff27c048c0f74edfccf51e98081bc6cc2be61c4
Half Man Half Amazing <ericmpadron@gmail.com>
2016-03-10 17:38:13 +0300

    fix square image issue

--- app/assets/javascripts/new-post.js
+++ app/assets/javascripts/new-post.js
@@ -27,8 +27,8 @@ $(function() {
         var yCrop;
 
         if (r == 1) {
-          newWidth = (cardWidth / width) * width;
-          newHeight = cardHeight;
+          newWidth = cardWidth;
+          newHeight = (cardWidth / width) * height;
 
           var w = (width / newWidth);
 
