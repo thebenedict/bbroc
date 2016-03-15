612f64e79b3d19cd5a24046834e40731c1eb9283
Half Man Half Amazing <ericmpadron@gmail.com>
2016-03-15 14:44:10 +0300

    request bar addition

--- app/assets/javascripts/site-nav.js
+++ app/assets/javascripts/site-nav.js
@@ -1,4 +1,17 @@
 // sign in and sign up toggle
-$('.toggle-container span a').on('click', function() {
-  $('.sign-up, .sign-in').toggleClass('hidden');
+$('.sign-up-toggle').on('click', function() {
+  $('.sign-up').toggleClass('show');
+  $('#item-requested').text($('#request-bar').val());
+});
+
+var initVal = "apples";
+$(document).ready(function(){
+    $(".sign-up-toggle").attr("disabled", "true");
+    $(".request-box").blur(function(){
+        if ($(this).val() != initVal && $(this).val() != "") {
+            $(".sign-up-toggle").removeAttr("disabled");
+        } else {
+            $(".sign-up-toggle").attr("disabled", "true");
+        }
+    });
 });
