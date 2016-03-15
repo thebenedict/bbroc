aeff3b63e31a052107ab2bf733e77c0351efb925
thebenedict <thebenedict@gmail.com>
2016-03-15 16:12:09 +0300

    finesse front page form

--- app/assets/javascripts/site-nav.js
+++ app/assets/javascripts/site-nav.js
@@ -3,15 +3,3 @@ $('.sign-up-toggle').on('click', function() {
   $('.sign-up').toggleClass('show');
   $('#item-requested').text($('#request-bar').val());
 });
-
-var initVal = "apples";
-$(document).ready(function(){
-    $(".sign-up-toggle").attr("disabled", "true");
-    $(".request-box").blur(function(){
-        if ($(this).val() != initVal && $(this).val() != "") {
-            $(".sign-up-toggle").removeAttr("disabled");
-        } else {
-            $(".sign-up-toggle").attr("disabled", "true");
-        }
-    });
-});
