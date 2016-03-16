36417eb541600e20dbb80e929ad57a47e50efe82
thebenedict <thebenedict@gmail.com>
2016-03-16 08:27:38 +0300

    fix request creation on fb sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
          :confirmable, :omniauthable, :omniauth_providers => [:facebook]
 
   has_many :posts
-  has_many :requests
+  has_many :requests, dependent: :destroy
 
   accepts_nested_attributes_for :requests
 
@@ -23,13 +23,14 @@ class User < ActiveRecord::Base
     self.requests.order(created_at: "DESC").limit(5)
   end
 
-  def self.from_omniauth(auth)
+  def self.from_omniauth(auth, params)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.skip_confirmation!
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
       user.name = auth.info.name
       user.hosted_avatar_url = auth.info.image
+      user.requests.build(body: params["request"])
     end
   end
 end
