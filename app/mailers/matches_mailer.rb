6322830a6b89b9dcfe2ffe63201cd0b1bce46553
Half Man Half Amazing <ericmpadron@gmail.com>
2016-03-17 17:20:50 +0300

    matches mialer start

--- app/mailers/matches_mailer.rb
+++ app/mailers/matches_mailer.rb
@@ -1,9 +1,10 @@
-class MatchesMailer < ActionMailer
+class MatchesMailer < ActionMailer::Base
   default from: "info@bestbroccoli.com"
   layout 'mailer'
 end
 
-def matches_email
+def matches_email(request)
+  @request = request
   opts[:from] = 'Team Best Broccoli <info@bestbroccoli.com>'
   opts[:reply_to] = 'info@bestbroccoli.com'
   opts[:subject] = 'We found it!'
