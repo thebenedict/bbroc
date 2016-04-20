1f8d0eb8be92bd41b056f5092711eb903fd01c82
Half Man Half Amazing <ericmpadron@gmail.com>
2016-04-20 15:15:56 +0300

    post options work on dynamic load

--- app/assets/javascripts/site-nav.js
+++ app/assets/javascripts/site-nav.js
@@ -4,11 +4,11 @@ $('.sign-up-toggle').on('click', function() {
   $('#item-requested').text($('#request-bar').val());
 });
 
-$('.post-toggle-options').on('click', function() {
-  if ($(this).hasClass('tapped') ) {
-    $('.post-toggle-options').removeClass('tapped');
-  }else {
-    $('.post-toggle-options').removeClass('tapped');
-    $(this).addClass('tapped');
-  }
-});
+$(document).on("click", ".post-toggle-options", function(){
+    if ($(this).hasClass('tapped') ) {
+      $('.post-toggle-options').removeClass('tapped');
+    }else {
+      $('.post-toggle-options').removeClass('tapped');
+      $(this).addClass('tapped');
+    }
+  });
