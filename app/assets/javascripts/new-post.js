810353f3688ba287010a3dccf703bf3eb049d166
Half Man Half Amazing <ericmpadron@gmail.com>
2016-05-06 17:48:23 +0300

    no canvas on new post

--- app/assets/javascripts/new-post.js
+++ app/assets/javascripts/new-post.js
@@ -6,57 +6,10 @@ $(function() {
         if (!file.type.match(imageType))
             return;
 
-        var reader = new FileReader();
-        reader.onload = fileOnload;
-        reader.readAsDataURL(file);
+
     });
 
-    function fileOnload(e) {
-        var $img = $('<img>', { src: e.target.result });
-      //var canvas = $('#canvas')[0];
-        var context = canvas.getContext('2d');
 
-      /*  var width = $img[0].width;
-		    var height = $img[0].height;
-        var r = (width / height);
-        var cardWidth = 305;
-        var cardHeight = 229;
-        var newWidth;
-		    var newHeight;
-        var xCrop;
-        var yCrop;
-
-        if (r == 1) {
-          newWidth = cardWidth;
-          newHeight = (cardWidth / width) * height;
-
-          var w = (width / newWidth);
-
-          xCrop = ((width / 2) - (cardWidth / 2) * w);
-          yCrop = 0;
-        } else if (r > 1) {
-          newHeight = cardHeight;
-          newWidth = (cardHeight / height) * width;
-
-          var w = (width / newWidth);
-
-          xCrop = ((width / 2) - (cardWidth / 2) * w);
-          yCrop = 0;
-        } else {
-          newWidth = cardWidth;
-          newHeight = (cardWidth / width) * height;
-
-          var h = (height / newHeight);
-
-          xCrop = 0;
-          yCrop = ((height / 2) - (cardHeight / 2) * h);
-        }*/
-
-        $img.load(function() {
-          var hiddenCanvas = $('#hidden-canvas')[0]
-          context.drawImage(this,0,0);
-        });
-    }
 
     // geolocation for new vendors
     function updateVendorDropdown(lat, lng) {
